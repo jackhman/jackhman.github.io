8c1273ade95c3fcc5a84664297b4cec172d22205
hongweifuture <hongweifuture@163.com>
2020-03-03 15:51:04 +0800

    add lab func from Github Actions at 2020-03-03 15:50:29

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 03 2020 15:44:42 GMT+0800 (China Standard Time)
+// build time:Tue Mar 03 2020 15:51:00 GMT+0800 (China Standard Time)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
