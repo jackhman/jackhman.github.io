8ed8a505398edd93a82c18dcb39c5b755477eea4
hongweifuture <hongweifuture@163.com>
2020-03-12 13:43:00 +0800

    add friend from Github Actions at 2020-03-12 13:42:24

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 12 2020 13:39:17 GMT+0800 (China Standard Time)
+// build time:Thu Mar 12 2020 13:42:57 GMT+0800 (China Standard Time)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
