b10fdb40b69d0f37122da2a62c436bd494851844
hongweifuture <hongweifuture@163.com>
2020-11-21 13:35:47 +0800

    update dependencies from Github Actions at 2020-11-21 13:34:50

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,5 @@
-// build time:Mon Jul 27 2020 14:43:28 GMT+0800 (China Standard Time)
-$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
-//rebuild by neat 
+// 代码块功能依赖
+
+$(function () {
+    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
+});
